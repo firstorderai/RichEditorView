c85c8a94b4885139b35a8b46455f11e6d694e059
grandon <grandon@firstorder.ai>
2025-11-03 16:03:41 +0800

    fix printer config set

--- RichEditorView/Sources/RichEditorView.swift
+++ RichEditorView/Sources/RichEditorView.swift
@@ -134,7 +134,7 @@ private let DefaultInnerLineHeight: Int = 21
         setup()
     }
     
-    private func printerWebViewConfiguration() {
+    private func printerWebViewConfiguration() -> WKWebViewConfiguration {
         let userScriptSource = """
             var style = document.createElement('style');
             style.innerHTML = `
